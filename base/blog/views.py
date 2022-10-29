cde046cdf9d3b8316df4e0af782ad14095f7c70c
Devin Slate <sl8t3@hotmail.com>
2022-10-28 22:45:35 -0600

    home view use context object

--- base/blog/views.py
+++ base/blog/views.py
@@ -3,5 +3,8 @@ from .models import Post
 
 
 class HomeView(generic.ListView):
+    """TODO: queryset or paginate approach"""
+
     model = Post
     template_name = "blog/home.html"
+    context_object_name = "post_list"
