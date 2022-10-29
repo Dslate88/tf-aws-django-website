cdc1c43c96bcdac6153219fa42170884de19cd34
Devin Slate <sl8t3@hotmail.com>
2022-10-29 13:02:29 -0600

    better url name for register

--- base/users/urls.py
+++ base/users/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from .views import UserRegister
 
 
-urlpatterns = [path("register/", UserRegister.as_view(), name="register")]
+urlpatterns = [path("register/", UserRegister.as_view(), name="user-register")]
