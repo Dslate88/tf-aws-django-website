288bac561a6267598c7e29bac0ee2ce67dd696e6
Devin Slate <sl8t3@hotmail.com>
2022-10-29 12:52:38 -0600

    updated urls for base and users for registration

--- base/base/urls.py
+++ base/base/urls.py
@@ -8,8 +8,11 @@ urls:
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("", include("blog.urls")),  # temporary
     path("admin/", admin.site.urls),
     path("blog/", include("blog.urls")),
+    path("users/", include("django.contrib.auth.urls")),
+    path("users/", include("users.urls")),
 ]
