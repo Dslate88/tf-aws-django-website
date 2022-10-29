6815a4d012423f35a9330e594038c92956497e23
Devin Slate <sl8t3@hotmail.com>
2022-10-29 11:20:20 -0600

    added author field to data model and template

--- base/blog/models.py
+++ base/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -6,6 +7,7 @@ class Post(models.Model):
     body = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
