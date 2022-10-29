2677783d8ca1fab52c2a118a9122f0d79380e29a
Devin Slate <sl8t3@hotmail.com>
2022-10-28 20:36:51 -0600

    added initial post data model with migraton

--- base/blog/models.py
+++ base/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    body = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
