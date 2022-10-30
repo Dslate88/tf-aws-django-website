fa3e926477737ca43211393c59f8a86ef6d1d9e9
Devin Slate <sl8t3@hotmail.com>
2022-10-29 20:06:20 -0600

    added login view to urlsconfig and linked to base html

--- base/users/urls.py
+++ base/users/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from .views import UserRegister
 
-
-urlpatterns = [path("register/", UserRegister.as_view(), name="user-register")]
+urlpatterns = [
+    path("register/", UserRegister.as_view(), name="user-register"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="users/login.html"),
+        name="user-login",
+    ),
+]
