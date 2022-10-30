35df440b77944f3d0f46838b4297b37b85d5857a
Devin Slate <sl8t3@hotmail.com>
2022-10-29 18:59:49 -0600

    added custom user register form to view

--- base/users/views.py
+++ base/users/views.py
@@ -1,9 +1,10 @@
 from django.views import generic
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm
+
+from .forms import UserRegisterForm
 
 
 class UserRegister(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = UserRegisterForm
     template_name = "users/register.html"
     success_url = reverse_lazy("login")
