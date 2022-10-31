243f915336a87fdec2119ef406d8b8e3e3f7c436
Devin Slate <sl8t3@hotmail.com>
2022-10-30 21:09:23 -0600

    remove upload_to, s3 later

--- base/users/models.py
+++ base/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="images", default="kairos_default.jpg")
+    image = models.ImageField(default="kairos_default.jpg")
 
     def __str__(self):
         return f"Profile:{self.user.username}"
