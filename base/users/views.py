e2fb52e0955f1787e4d1f55944e41988e8fdaa80
Devin Slate <sl8t3@hotmail.com>
2022-11-03 20:08:30 -0600

    explicit context management multiple forms

--- base/users/views.py
+++ base/users/views.py
@@ -4,8 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 
-# from base.users.models import Profile
-
+# from users.models import Profile
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
@@ -22,7 +21,8 @@ class UserRegisterView(SuccessMessageMixin, generic.CreateView):
 
 class UserProfileView(LoginRequiredMixin, generic.ListView):
     """
-    Render form and return http response for Profile access
+    Render form
+    Return http response for Profile access
 
     TODO: finish, include image upload, backend s3
     TODO: needs custom data model to handle additional fields into forms?
@@ -34,11 +34,17 @@ class UserProfileView(LoginRequiredMixin, generic.ListView):
 
 
 class UserProfileUpdateView(LoginRequiredMixin, generic.UpdateView):
-    """ """
-
-    # need to manually create context here...merge user form fields and render to html?
+    """
+    Render user and profile forms
+    """
 
     model = User
     template_name = "users/profile_update.html"
     success_url = reverse_lazy("user-profile")
     form_class = UserUpdateForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user_form"] = UserUpdateForm()
+        context["profile_form"] = ProfileUpdateForm()
+        return context
