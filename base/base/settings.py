8d81fec27c00ad635d2f098ca15c00ac338f93d9
Devin Slate <sl8t3@hotmail.com>
2022-11-06 11:58:03 -0700

    env file rename for dev

--- base/base/settings.py
+++ base/base/settings.py
@@ -23,7 +23,7 @@ env = environ.Env(
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env.webapp"))
 
 
 ALLOWED_HOSTS = []
