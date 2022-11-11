ca2aa94d7faaff8176fed5005d4e978c73d89fa3
Devin Slate <sl8t3@hotmail.com>
2022-11-10 18:39:31 -0700

    added vpc module

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,46 @@
+locals {
+  stack_name = "tf_aws_django_website"
+  env        = "dev"
+
+  # vpc
+  vpc_name             = "website"
+  vpc_cidr             = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_igw           = true
+
+  # subnet pub: evens
+  pub_cidrs       = ["10.0.0.0/24", "10.0.2.0/24"]
+  pub_avail_zones = ["us-east-1a", "us-east-1b"]
+  pub_map_ip      = true
+
+  # subnet priv: odds
+  priv_cidrs       = ["10.0.1.0/24", "10.0.3.0/24"]
+  priv_avail_zones = ["us-east-1a", "us-east-1b"]
+  priv_nat_gateway = false # for now
+}
+
+module "vpc" {
+  source     = "git@github.com:Dslate88/tf-aws-vpc"
+  stack_name = local.stack_name
+  env        = local.env
+
+  # vpc
+  vpc_name             = local.vpc_name
+  vpc_cidr             = local.vpc_cidr
+  enable_dns_hostnames = local.enable_dns_hostnames
+  enable_igw           = local.enable_igw
+
+  # subnet public
+  pub_cidrs       = local.pub_cidrs
+  pub_avail_zones = local.pub_avail_zones
+  pub_map_ip      = local.pub_map_ip
+
+  # subnet private
+  priv_cidrs       = local.priv_cidrs
+  priv_avail_zones = local.priv_avail_zones
+  priv_nat_gateway = local.priv_nat_gateway
+}
+
 # 1 repo for each compose container
 resource "aws_ecr_repository" "webapp" {
   name                 = "django_webapp"
