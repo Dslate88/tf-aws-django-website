fdd53261618be67e93c3da51822d2f9563c332a7
Devin Slate <sl8t3@hotmail.com>
2022-11-11 14:33:29 -0700

    explicit naming for execution_role

--- tf/main.tf
+++ tf/main.tf
@@ -95,7 +95,7 @@ resource "aws_ecs_cluster_capacity_providers" "example" {
   }
 }
 
-resource "aws_iam_role" "task" {
+resource "aws_iam_role" "ecs_task_execution_role" {
   name = "${local.stack_name}-ecs-task-execution-role-${local.env}"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -111,9 +111,9 @@ resource "aws_iam_role" "task" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "task" {
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   # TODO:  # scope permissions down
-  role       = aws_iam_role.task.name
+  role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
