0269f320748c61f0bd8fc35e81b70de00d23e897
Devin Slate <sl8t3@hotmail.com>
2022-11-11 10:20:05 -0700

    add iam role/policy to the task definition resource

--- tf/main.tf
+++ tf/main.tf
@@ -1,6 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-
 locals {
   stack_name = "django_website"
   env        = "dev"
@@ -80,9 +79,32 @@ resource "aws_ecs_cluster_capacity_providers" "example" {
   }
 }
 
+resource "aws_iam_role" "task" {
+  name = "${local.stack_name}-ecs-task-execution-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "task" {
+  # TODO:  # scope permissions down
+  role       = aws_iam_role.task.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "webapp" {
   family                   = "test_task"
   requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.task.arn
   network_mode             = "awsvpc"
   cpu                      = 256
   memory                   = 512
