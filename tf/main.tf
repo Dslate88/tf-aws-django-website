f57b5fac450207785e949850c4e91166b747b7ec
Devin Slate <sl8t3@hotmail.com>
2022-11-12 01:50:13 -0700

    check in test more

--- tf/main.tf
+++ tf/main.tf
@@ -10,12 +10,16 @@ locals {
   enable_dns_hostnames = true
   enable_igw           = true
 
-  pub_cidrs       = ["10.0.0.0/24", "10.0.2.0/24"]
-  pub_avail_zones = ["us-east-1a", "us-east-1b"]
+  # pub_cidrs       = ["10.0.0.0/24", "10.0.2.0/24"]
+  # pub_avail_zones = ["us-east-1a", "us-east-1b"]
+  pub_cidrs       = ["10.0.0.0/24"]
+  pub_avail_zones = ["us-east-1a"]
   pub_map_ip      = true
 
-  priv_cidrs       = ["10.0.1.0/24", "10.0.3.0/24"]
-  priv_avail_zones = ["us-east-1a", "us-east-1b"]
+  priv_cidrs       = ["10.0.1.0/24"]
+  priv_avail_zones = ["us-east-1a"]
+  # priv_cidrs       = ["10.0.1.0/24", "10.0.3.0/24"]
+  # priv_avail_zones = ["us-east-1a", "us-east-1b"]
   priv_nat_gateway = true
 
   enable_s3_endpoint      = false
@@ -85,190 +89,152 @@ resource "aws_ecr_lifecycle_policy" "webapp" {
 }
 
 
-resource "aws_ecs_cluster" "main" {
-  # TODO: add cloudwatch/s3 log storage to configuration, if I need execute_command?
-  name = "${local.stack_name}-cluster-${local.env}"
 
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
-}
-
-resource "aws_ecs_cluster_capacity_providers" "example" {
-  cluster_name       = aws_ecs_cluster.main.name
-  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
-
-  # TODO: use spot instances here??
-  default_capacity_provider_strategy {
-    base              = 1
-    weight            = 100
-    capacity_provider = "FARGATE"
-  }
-}
-
-resource "aws_security_group" "alb-internal" {
-  name   = "${local.stack_name}-sg-alb-${local.env}"
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 443
-    to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "ecs_tasks" {
-  name   = "${local.stack_name}-sg-task-${local.env}"
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80 # TODO:
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "${local.stack_name}-ecs-task-execution-role-${local.env}"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
-  # TODO:  # scope permissions down
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_ecs_task_definition" "main" {
-  # TODO: add task_role_arn with needed perms...
-  family = "test_annoyed_9371"
-  # family                   = "${local.stack_name}-${local.env}"
-  requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  network_mode             = "awsvpc"
-  cpu                      = 256
-  memory                   = 512
-  # TODO: dynamnic generate containers...
-  container_definitions = jsonencode([
-    {
-      name      = "nginx"
-      image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com/django_nginx"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          protocol      = "tcp"
-        }
-      ]
-    },
-    {
-      name      = "django"
-      image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com/django_webapp"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 8000
-          protocol      = "tcp"
-        }
-      ]
-    }
-    ]
-  )
-
-  runtime_platform {
-    operating_system_family = "LINUX"
-    cpu_architecture        = "ARM64"
-  }
-}
-
-resource "aws_ecs_service" "main" {
-  name            = "${local.stack_name}-service-${local.env}"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.main.arn
-  launch_type     = "FARGATE"
-  desired_count   = 1
-  # deployment_maximum_percent         = 200
-  # deployment_minimum_healthy_percent = 100
-  scheduling_strategy = "REPLICA"
-
-  deployment_controller {
-    type = "ECS"
-  }
-
-  # TODO: add security_groups
-  # TODO: convert to priv_subs and use alb
-  network_configuration {
-    subnets          = module.vpc.pub_subnets
-    assign_public_ip = true
-    security_groups  = [aws_security_group.allow-external.id]
-  }
-}
-
-resource "aws_security_group" "allow-external" {
-  vpc_id      = module.vpc.vpc_id
-  name        = "${local.stack_name}-${local.env}-allow-external-ecs"
-  description = "Allows external traffic"
-
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = "0"
-    to_port     = "0"
-    protocol    = "-1"
-  }
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = "443"
-    to_port     = "443"
-    protocol    = "tcp"
-  }
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = "8000"
-    to_port     = "8000"
-    protocol    = "tcp"
-  }
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = "80"
-    to_port     = "80"
-    protocol    = "tcp"
-  }
-}
+# resource "aws_security_group" "ecs_tasks" {
+#   name   = "${local.stack_name}-sg-task-${local.env}"
+#   vpc_id = module.vpc.vpc_id
+#
+#   ingress {
+#     protocol    = "tcp"
+#     from_port   = 80
+#     to_port     = 80
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   egress {
+#     protocol    = "-1"
+#     from_port   = 0
+#     to_port     = 0
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+#
+# resource "aws_security_group" "alb" {
+#   name   = "${local.stack_name}-sg-alb-${local.env}"
+#   vpc_id = module.vpc.vpc_id
+#
+#   ingress {
+#     protocol    = "tcp"
+#     from_port   = 80
+#     to_port     = 80
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   ingress {
+#     protocol    = "tcp"
+#     from_port   = 443
+#     to_port     = 443
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   egress {
+#     protocol    = "-1"
+#     from_port   = 0
+#     to_port     = 0
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+#
+#
+# resource "aws_ecs_cluster" "main" {
+#   # TODO: add cloudwatch/s3 log storage to configuration, if I need execute_command?
+#   name = "${local.stack_name}-cluster-${local.env}"
+#
+#   setting {
+#     name  = "containerInsights"
+#     value = "enabled"
+#   }
+# }
+#
+# resource "aws_ecs_cluster_capacity_providers" "example" {
+#   cluster_name       = aws_ecs_cluster.main.name
+#   capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+#
+#   # TODO: use spot instances here??
+#   default_capacity_provider_strategy {
+#     base              = 1
+#     weight            = 100
+#     capacity_provider = "FARGATE"
+#   }
+# }
+#
+#
+#
+#
+# resource "aws_ecs_task_definition" "main" {
+#   # TODO: add task_role_arn with needed perms...
+#   # family                   = "${local.stack_name}-${local.env}"
+#   family                   = "test_annoyed_9821"
+#   requires_compatibilities = ["FARGATE"]
+#   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+#   task_role_arn            = aws_iam_role.ecs_task_role.arn
+#   network_mode             = "awsvpc"
+#   cpu                      = 256
+#   memory                   = 512
+#   # TODO: dynamnic generate containers...
+#   container_definitions = jsonencode([
+#     {
+#       name      = "nginx"
+#       image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com/django_nginx"
+#       essential = true
+#       portMappings = [
+#         {
+#           containerPort = 80
+#           protocol      = "tcp"
+#         }
+#       ]
+#     },
+#     {
+#       name      = "django"
+#       image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com/django_webapp"
+#       essential = true
+#       portMappings = [
+#         {
+#           containerPort = 8000
+#           protocol      = "tcp"
+#         }
+#       ]
+#     }
+#     ]
+#   )
+#
+#   runtime_platform {
+#     operating_system_family = "LINUX"
+#     cpu_architecture        = "ARM64"
+#   }
+# }
+#
+#
+# resource "aws_ecs_service" "main" {
+#   name                               = "${local.stack_name}-service-${local.env}"
+#   cluster                            = aws_ecs_cluster.main.id
+#   task_definition                    = aws_ecs_task_definition.main.arn
+#   launch_type                        = "FARGATE"
+#   desired_count                      = 2
+#   scheduling_strategy                = "REPLICA"
+#   deployment_maximum_percent         = 200
+#   deployment_minimum_healthy_percent = 50
+#
+#   deployment_controller {
+#     type = "ECS"
+#   }
+#
+#   # load_balancer {
+#   #   target_group_arn = aws_lb_target_group.main.arn
+#   #   container_name   = "nginx"
+#   #   container_port   = 80
+#   # }
+#
+#   # TODO: convert to priv_subs and use alb
+#   network_configuration {
+#     # subnets          = module.vpc.priv_subnets
+#     # assign_public_ip = false
+#     subnets          = module.vpc.pub_subnets
+#     assign_public_ip = true
+#     security_groups  = [aws_security_group.ecs_tasks.id, aws_security_group.alb.id]
+#   }
+#
+#   lifecycle {
+#     ignore_changes = [desired_count, task_definition]
+#   }
+# }
