cb3084b851d26bb52545501b893c7819daa5cdbb
Devin Slate <sl8t3@hotmail.com>
2022-11-16 23:07:56 -0700

    enable alb health check

--- infra/fargate/alb.tf
+++ infra/fargate/alb.tf
@@ -4,13 +4,14 @@ resource "aws_lb_target_group" "main" {
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
   target_type = "ip"
-  # health_check {
-  #   path                = "/"
-  #   interval            = 30
-  #   timeout             = 5
-  #   healthy_threshold   = 2
-  #   unhealthy_threshold = 2
-  # }
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb" "main" {
@@ -21,29 +22,30 @@ resource "aws_lb" "main" {
   subnets            = var.pub_subnet_ids
 }
 
-resource "aws_lb_listener" "main" {
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.cert.arn
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.main.arn
   }
 }
-
-# create listener rule
-# resource "aws_lb_listener_rule" "main" {
-#   listener_arn = aws_lb_listener.main.arn
-#   priority     = 1
-#
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.main.arn
-#   }
-#
-#   condition {
-#     field  = "path-pattern"
-#     values = ["/"]
-#   }
-# }
